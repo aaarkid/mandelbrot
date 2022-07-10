3f662155e49b8c322da7f1232e4c95a6d3bef070
aaarkid <kaleciarkid@gmail.com>
2022-07-10 13:52:05 +0200

    function to compute escape time for point in graph

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use num;
+use std::str::FromStr;
+
+
+
+fn escape_time(c: Complex<f64>, limit: u8) -> Option<u8> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i: u8 in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+    None
+}
+
 fn main() {
     println!("Hello, world!");
 }
