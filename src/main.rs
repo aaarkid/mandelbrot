f04f6745bd7bb09e2806900fa9d4676e0118df29
aaarkid <kaleciarkid@gmail.com>
2022-07-10 17:57:18 +0200

    parse resolution input from cli

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,18 @@ fn escape_time(c: Complex<f64>, limit: u8) -> Option<u8> {
     None
 }
 
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
