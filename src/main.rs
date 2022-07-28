d2610e364037e9a04f7ebc73c479b46cddf21e15
Arkid Kaleci <kaleciarkid@gmail.com>
2022-07-28 14:18:03 +0200

    fixed deprecated functions from image crate

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use num::Complex;
 use std::str::FromStr;
 use image::ColorType;
-use image::png::PNGEncoder;
+use image::png::PngEncoder;
 use std::fs::File;
 use num_cpus;
 
@@ -72,8 +72,8 @@ fn render(pixels: &mut [u8],
 
 fn write_image (filename: &str, pixels: &[u8], bounds: (usize, usize)) ->  Result<(), std::io::Error> {
     let output = File::create(filename)?;
-    let encoder = PNGEncoder::new(output);
-    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+    let encoder = PngEncoder::new(output);
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::L8).unwrap();
 
     Ok(())
 }
